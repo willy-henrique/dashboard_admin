f06c3658dda852457eabf897ffb5ee7356911e33
willy <evogame100@gmail.com>
2025-09-16 10:54:46 -0300

    fix(build): importar Skeleton corretamente para evitar erro de prerender

--- components/users/users-table.tsx
+++ components/users/users-table.tsx
@@ -7,6 +7,7 @@ import { Badge } from "@/components/ui/badge"
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Skeleton } from "@/components/ui/skeleton"
 import { 
   Search, 
   Filter, 
